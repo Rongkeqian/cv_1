16f7d8509190403ebb505a289b8b1b4d0dfbe209
RongKeQian <qianer_lover@126.com>
2020-07-22 23:07:15 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -1,26 +1,70 @@
-let demo = document.querySelector("#demo");
-let string = `
-你好，我是一名前端新人
-接下来我要加样式了
-我要加的样式是
-body{
-    color:red;
-}`;
+let html = document.querySelector("#html");
+let style = document.querySelector("#style");
+let string = `/*你好，我是大倩倩
+*接下来我演示一下我的前端功底
+*首先我要准备一个div
+*/
+#div1{
+    border:1px solid red;
+    width:200px;
+    height:200px;
+}
+/*接下来我把div变成一个八卦图
+*注意看好了
+*首先，把div变成一个圆
+*/
+#div1{
+  border-radius:50%;
+  box-shadow:0 0 3px rgba(0,0,0,0.5);
+  border:none;
+}
+/*八卦是阴阳形成的
+*一黑一白
+*/
+#div1{
+  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/*加两个小球*/
+#div1::before{
+  width:100px;
+  height:100px;
+  border-radius:50%;
+  top:0;
+  left:50%;
+  transform:translateX(-50%);
+  background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 23%, rgba(0,0,0,1) 23%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+  width:100px;
+  height:100px;
+  border-radius：50%;
+  bottom:0;
+  left:50%;
+  transform:translateX(-50%);
+  border-radius:50%;
+  background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(43,43,43,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+}
+`;
 let string2 = "";
 console.log(string.length);
 let n = 0;
 
 let step = () => {
   setTimeout(() => {
-    console.log(n);
+    //如果是回车，就不照搬
+    //如果不是回车就照搬
     if (string[n] === "\n") {
-      //如果是回车，就不照搬
       string2 += "<br>";
+    } else if (string[n] === " ") {
+      string2 += "&nbsp;";
     } else {
-      //如果不是回车就照搬
       string2 += string[n];
     }
-    demo.innerHTML = string2;
+
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n);
+    window.scrollTo(0, 9999);
+    html.scrollTo(0, 99999);
     if (n < string.length - 1) {
       //不是最后一个,就继续
       n += 1;
@@ -28,6 +72,6 @@ let step = () => {
     } else {
       //n是最后一个
     }
-  }, 100);
+  }, 0);
 };
 step();
